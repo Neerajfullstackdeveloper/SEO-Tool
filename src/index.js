2b020ea25a500af52c6c3918cca3a95fad72528a
Neeraj Pandey <137383536+Neerajfullstackdeveloper@users.noreply.github.com>
2025-04-28 11:28:13 +0530

    index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import { BrowserRouter } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-    <BrowserRouter basename='SEO-Tool'>
+    <BrowserRouter basename='/SEO-Tool'>
     <App />
  </BrowserRouter>
 );
