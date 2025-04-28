a9cf5eeeb6fc30ee972c7bca60a2c903474a55f1
Neeraj Pandey <137383536+Neerajfullstackdeveloper@users.noreply.github.com>
2025-04-28 11:21:25 +0530

    App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import BusinessForm from "./components/FormPage1";
 import ResultPage from "./components/Page2Display";
 
 const App = () => {
   return (
-    <>
+    <BrowserRouter >
       <Routes>
         <Route path="/" element={<BusinessForm />} > </Route>
         <Route path="/result" element={<ResultPage />} > </Route>
       </Routes>
-    </>
+    </BrowserRouter>
   );
 };
 
